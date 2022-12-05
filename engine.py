c50a7bc7d9757fa005a7b5e184f1befbbb1aa741
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2022-12-05 22:44:08 +0100

    usuwanie z inwentarza

--- engine.py
+++ engine.py
@@ -25,4 +25,31 @@ def put_player_on_board(board, player):
     '''
     cords = player['player_cord']
     board[cords[0]][cords[1]] = player['player_symbol']
-    
+
+
+# def go_through_door(location):
+#     if player #obok zablokowaneych drzwi i naciska klawisz w stronę drzwi
+#         if inventory[key] != None:
+#             #otwórz drzwi i zapamiętaj - zmiana na planszy
+#             #zmień planszę
+#         else:
+#             print('You need a key to open the door')
+
+
+# def add_to_inventory(player, inventory, item):
+#     player = main.create_player()
+#     items = {
+#         "k": 'keys',
+#         "w" : 'weapon',
+#         "a" : 'armor',
+#         "m" : 'medicine'
+#     }
+#     if player[1]
+       
+import util
+
+def delete_from_inventory(inventory):
+    my_key = util.key_pressed()
+    if my_key == 'm':
+        inventory['medicine'] -= 1
+
