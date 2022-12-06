31ec44f2530761f47a12ad2b85abc553340433d2
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2022-12-06 20:47:52 +0100

    take item, add item, use item, delete item
    dodaje 4 funkcje, dwie dodatkowe dodane jako na razie zwracające prawdę

--- engine.py
+++ engine.py
@@ -1,3 +1,5 @@
+import util
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -27,29 +29,52 @@ def put_player_on_board(board, player):
     board[cords[0]][cords[1]] = player['player_symbol']
 
 
-# def go_through_door(location):
-#     if player #obok zablokowaneych drzwi i naciska klawisz w stronę drzwi
-#         if inventory[key] != None:
+# def go_through_door(player, board, player_coord, inventory):
+#     my_key = util.key_pressed()
+#     if board[player_coord[1]] - 1 ==  #obok zablokowaneych drzwi i naciska klawisz w stronę drzwi
+#         if inventory['key'] != None and my_key == 'k':
+#             delete_from_inventory(inventory['key'])
 #             #otwórz drzwi i zapamiętaj - zmiana na planszy
 #             #zmień planszę
 #         else:
 #             print('You need a key to open the door')
 
 
-# def add_to_inventory(player, inventory, item):
-#     player = main.create_player()
-#     items = {
-#         "k": 'keys',
-#         "w" : 'weapon',
-#         "a" : 'armor',
-#         "m" : 'medicine'
-#     }
-#     if player[1]
-       
-import util
+def take_item(player, item):
+    if player['player_cords'] == item['cords']:
+        ask_for_adding(item)
 
-def delete_from_inventory(inventory):
+
+def ask_for_adding(item):
+    return True
+
+def add_to_inventory(inventory, item):
+    permission = ask_for_adding()
+    if permission:
+        inventory[item] += 1
+    return inventory
+
+
+
+
+def use_item(inventory):
     my_key = util.key_pressed()
-    if my_key == 'm':
-        inventory['medicine'] -= 1
+    using_items = {
+        "k": 'keys',
+        "m" : 'medicine'
+    }
+    if str(my_key) in using_items:
+        if input('Do you want to use one of your ', using_items[my_key], "? Confirm by c"):
+            delete_from_inventory(inventory, using_items[str(my_key)])
 
+
+
+
+def ask_for_using(item):
+    return True
+
+
+
+def delete_from_inventory(inventory, item):
+    inventory[item] -=1
+    return inventory
