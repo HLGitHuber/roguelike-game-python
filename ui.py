ae7fbdb2876f13d9b4f559555a4cdca961eaae45
HLGitHuber <hubertlewandowski2002@gmail.com>
2022-12-10 23:36:07 +0100

    latest ui update

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+
 def display_board(board, color_scheme):
     '''
     Displays complete game board on the screen
@@ -5,13 +6,42 @@ def display_board(board, color_scheme):
     Returns:
     Nothing
     '''
+    # row_count = 0
+    # element_count = 0
     for row in board:
         for element in row:
             try:
+                '''
+                try:
+                    if board[row_count+1][element_count] == '#' and row[element_count+1] == '#':
+                        temp = color_scheme[element]
+                        temp= '\033[0;34;40m'u'\u2554''\033[0;37;40m'
+                        print(temp, end='')
+                    if board[row_count+1][element_count] == '#' and row[element_count-1] == '#':
+                        temp = color_scheme[element]
+                        temp= '\033[0;34;40m'u'\u2557''\033[0;37;40m'
+                        print(temp, end='')
+                    if board[row_count-1][element_count] == '#' and row[element_count+1] == '#':
+                        temp = color_scheme[element]
+                        temp= '\033[0;34;40m'u'\u255A''\033[0;37;40m'
+                        print(temp, end='')
+                    if board[row_count-1][element_count] != '#' and board[row_count+1][element_count] != '#':
+                        temp = color_scheme[element]
+                        temp= '\033[0;34;40m'u'\u2550''\033[0;37;40m'
+                        print(temp, end='')
+                    if row[element_count-1] != '#' and row[element_count+1] != '#':
+                        temp = color_scheme[element]
+                        temp= '\033[0;34;40m'u'\u2551''\033[0;37;40m'
+                        print(temp, end='')
+                except:
+                '''
                 print(color_scheme[element], end = '')
+                
             except KeyError:
                 print(element, end='')
+            # element_count += 1
         print()
+        # row_count += 1
 
 def display_stats(statistics):
     # statistics - dict
